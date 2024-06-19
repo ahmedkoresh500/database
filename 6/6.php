182a45e65f15c73cc18b8b3bda5b138488cc0c12
ahmedkoresh500 <ahmedkoresh500@gmail.com>
2024-06-19 18:31:17 +0300

    commit 6

--- 6/6.php
+++ 6/6.php
@@ -12,25 +12,25 @@
 */
 
 $dsn = "mysql:host=localhost;dbName=test";
-$user = "root";
+$userName = "root";
 $password = "";
 
 $options = array(
-    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8";
-);
+    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8";       // [utf8]  =>> uppercase or lowercase
+);                                                          // [utf8]  =>> support arabic in database
 
 try{
-    $db = new PDO($dsn, $user, $password, $options);
+    $db = new PDO($dsn, $userName, $password, $options);
     $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(PDOException $e){
     echo "Failed" . $e -> getMessage();
 };
 
 /*
-    $stm = $db -> prepare("");    =>> two are the same
+    $stm = $db -> prepare("");      =>> two are the same
     $stm -> execute();
 
-    $query = "";                =>> two are the same
+    $query = "";                    =>> two are the same
     $db -> exec($query);
 */
 
